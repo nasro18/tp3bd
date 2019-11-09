54979ca6dc2105e6e426a7d0a012d537fd9c28f9
nasro18 <57290616+nasro18@users.noreply.github.com>
2019-11-09 15:07:22 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+	
+    		$bd= new PDO('mysql:host=localhost;dbname=bddsi22-tp3;charset=utf8', 'root', '');
+	
+     
+
+
+?>
+
+    
+</body>
+</html>
